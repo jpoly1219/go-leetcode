2721415e5f63996b35e08dde2aa6ba550a612986
Jacob Kim <jpoly1219@gmail.com>
2021-07-31 14:43:06 +0000

    implemented concurrent prints of hello and goodbye

--- pkg/controllers.go
+++ pkg/controllers.go
@@ -1,4 +1,4 @@
-package controllers
+package pkg
 
 import (
 	"fmt"
@@ -7,5 +7,23 @@ import (
 
 func SayHello(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("endpoint reached")
-	w.Write([]byte("Hello world!\n"))
+
+	out1 := make(chan string)
+	out2 := make(chan string)
+
+	go PrintHello(5, out1)
+	go PrintBye(5, out2)
+
+	for i := 0; i < 10; i++ {
+		select {
+		case msg := <-out1:
+			fmt.Print(msg)
+			fmt.Fprint(w, msg)
+		case msg := <-out2:
+			fmt.Print(msg)
+			fmt.Fprint(w, msg)
+		}
+	}
+
+	fmt.Fprintf(w, "done\n")
 }
