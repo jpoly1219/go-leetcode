70c534b815ad5ef337b9943315b2f4c1620c379e
Jacob Kim <jpoly1219@gmail.com>
2021-07-31 13:46:49 +0000

    added a route handler using gorilla/mux

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/jpoly1219/go-leetcode/controllers"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	r := mux.NewRouter()
+	r.HandleFunc("/run", controllers.SayHello)
+
+	log.Fatal(http.ListenAndServe(":8090", r))
 }
