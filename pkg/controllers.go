8a7282d2fd7226a1385d84d9936c467c44b5563c
Jacob Kim <jpoly1219@gmail.com>
2021-08-01 07:09:35 +0000

    implemented filepath generation and passing that to runCpp with user input

--- pkg/controllers.go
+++ pkg/controllers.go
@@ -2,28 +2,37 @@ package pkg
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"path/filepath"
 )
 
 func SayHello(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("endpoint reached")
 
-	out1 := make(chan string)
-	out2 := make(chan string)
+	baseDir := LoadEnv("BASEDIR")
+	userfilesDir := filepath.Join(baseDir, "userfiles")
+	content, _ := ioutil.ReadFile(filepath.Join(userfilesDir, "test.cpp"))
+	runCpp(content, userfilesDir)
 
-	go PrintHello(5, out1)
-	go PrintBye(5, out2)
+	/*
+		out1 := make(chan string)
+		out2 := make(chan string)
 
-	for i := 0; i < 10; i++ {
-		select {
-		case msg := <-out1:
-			fmt.Print(msg)
-			fmt.Fprint(w, msg)
-		case msg := <-out2:
-			fmt.Print(msg)
-			fmt.Fprint(w, msg)
+		go PrintHello(5, out1)
+		go PrintBye(5, out2)
+
+		for i := 0; i < 10; i++ {
+			select {
+			case msg := <-out1:
+				fmt.Print(msg)
+				fmt.Fprint(w, msg)
+			case msg := <-out2:
+				fmt.Print(msg)
+				fmt.Fprint(w, msg)
+			}
 		}
-	}
 
-	fmt.Fprintf(w, "done\n")
+		fmt.Fprintf(w, "done\n")
+	*/
 }
