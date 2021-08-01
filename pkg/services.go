f276c5331c4d5ce1b787a8df22aaca049e245591
Jacob Kim <jpoly1219@gmail.com>
2021-08-01 07:10:05 +0000

    implementing a feature where runCpp will dynamically generate a cpp file to compile and run

--- pkg/services.go
+++ pkg/services.go
@@ -1,9 +1,54 @@
 package pkg
 
 import (
-	"time"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
 )
 
+func LoadEnv(key string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return os.Getenv(key)
+}
+
+func runCpp(input []byte, userfileDir string) {
+	fileId := uuid.New()
+	fileName := fmt.Sprintf("%s.cpp", fileId)
+	pathToFile := filepath.Join(userfileDir, fileName)
+
+	cmd := exec.Command("touch", pathToFile)
+	fmt.Println("creating file")
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmdInsertInput := fmt.Sprintf("%s > %s.cpp", string(input), fileId)
+	cmd = exec.Command(cmdInsertInput)
+	fmt.Println("inserting input")
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmdCompileAndRun := fmt.Sprintf("g++ %s -o %s; ./%s", pathToFile, fileId, fileId)
+	fmt.Println("compiling and running")
+	out, err := exec.Command(cmdCompileAndRun).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("output:\n", string(out))
+}
+
+/*
 func PrintHello(num int, out chan string) {
 	defer close(out)
 	for i := 0; i < num; i++ {
@@ -19,3 +64,4 @@ func PrintBye(num int, out chan string) {
 		time.Sleep(time.Second)
 	}
 }
+*/
