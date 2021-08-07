5ebf3a070c1afdce205143e9974d5dee01bc826c
Jacob Kim <jpoly1219@gmail.com>
2021-08-07 13:13:10 +0000

    removed unnecessary comments

--- pkg/controllers.go
+++ pkg/controllers.go
@@ -19,24 +19,4 @@ func SayHello(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Write([]byte(outCpp))
 
-	/*
-		out1 := make(chan string)
-		out2 := make(chan string)
-
-		go PrintHello(5, out1)
-		go PrintBye(5, out2)
-
-		for i := 0; i < 10; i++ {
-			select {
-			case msg := <-out1:
-				fmt.Print(msg)
-				fmt.Fprint(w, msg)
-			case msg := <-out2:
-				fmt.Print(msg)
-				fmt.Fprint(w, msg)
-			}
-		}
-
-		fmt.Fprintf(w, "done\n")
-	*/
 }
