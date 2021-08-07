f7e8af3f18934a7af16dc13cb10385c90ba28722
Jacob Kim <jpoly1219@gmail.com>
2021-08-07 13:24:00 +0000

    SayHello controller now returns Python outputs as well

--- pkg/controllers.go
+++ pkg/controllers.go
@@ -12,11 +12,19 @@ func SayHello(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("endpoint reached")
 
 	userfilesDir := filepath.Join(".", "userfiles/")
+
 	content, _ := ioutil.ReadFile(filepath.Join(userfilesDir, "test.cpp"))
 	outCpp, err := runCpp(content, userfilesDir)
 	if err != nil {
 		log.Fatal(err)
 	}
 	w.Write([]byte(outCpp))
+
+	content, _ = ioutil.ReadFile(filepath.Join(userfilesDir, "test.py"))
+	outPy, err := runPy(content, userfilesDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write([]byte(outPy))
 
 }
