6011b87305be01a1f4447c0c29ea5c16b5f87559
Jacob Kim <jpoly1219@gmail.com>
2021-08-08 03:25:05 +0000

    Run() controller can now handle Java, JS, and Python

--- pkg/controllers.go
+++ pkg/controllers.go
@@ -11,20 +11,33 @@ import (
 func Run(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("endpoint reached")
 
-	userfilesDir := filepath.Join(".", "userfiles/")
+	pathUserfiles := filepath.Join(".", "userfiles", "testuserfiles")
 
-	content, _ := ioutil.ReadFile(filepath.Join(userfilesDir, "test.cpp"))
-	outCpp, err := runCpp(content, userfilesDir)
+	content, _ := ioutil.ReadFile(filepath.Join(pathUserfiles, "test.cpp"))
+	outCpp, err := runCpp(content, pathUserfiles)
 	if err != nil {
 		log.Fatal(err)
 	}
 	w.Write([]byte(outCpp))
 
-	content, _ = ioutil.ReadFile(filepath.Join(userfilesDir, "test.py"))
-	outPy, err := runPy(content, userfilesDir)
+	content, _ = ioutil.ReadFile(filepath.Join(pathUserfiles, "test.java"))
+	outJava, err := runJava(content, pathUserfiles)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write([]byte(outJava))
+
+	content, _ = ioutil.ReadFile(filepath.Join(pathUserfiles, "test.js"))
+	outJs, err := runJs(content, pathUserfiles)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write([]byte(outJs))
+
+	content, _ = ioutil.ReadFile(filepath.Join(pathUserfiles, "test.py"))
+	outPy, err := runPy(content, pathUserfiles)
 	if err != nil {
 		log.Fatal(err)
 	}
 	w.Write([]byte(outPy))
-
 }
