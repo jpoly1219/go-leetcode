98ddbe3e017cb324d72cb483ffaf5d045c942d30
Jacob Kim <jpoly1219@gmail.com>
2021-08-11 14:21:07 +0000

    writing my first test for services.go

---
+++ tests/services_test.go
@@ -0,0 +1,13 @@
+package tests
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/jpoly1219/go-leetcode/pkg"
+)
+
+func TestRunCpp(t *testing.T) {
+	output, err := RunCpp(input, pathUserfiles)
+	if 
+}
