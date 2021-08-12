08590c5ecb127194506279b37e7cf8faea5f9748
Jacob Kim <jpoly1219@gmail.com>
2021-08-12 13:03:10 +0000

    created test for RunJava()

--- tests/services_test.go
+++ tests/services_test.go
@@ -16,3 +16,12 @@ func TestRunCpp(t *testing.T) {
 		t.Errorf("expected 'running test.cpp'")
 	}
 }
+
+func TestRunJava(t *testing.T) {
+	pathUserfiles := filepath.Join(".", "testuserfiles")
+	input, _ := ioutil.ReadFile(filepath.Join(pathUserfiles, "test.java"))
+	output, _ := pkg.RunJava(input, pathUserfiles)
+	if output != "running test.java\n" {
+		t.Errorf("expected 'running test.java'")
+	}
+}
