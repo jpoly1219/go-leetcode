7b8b1abbf93d2a944945fb8b5787cac4a1785d73
Jacob Kim <jpoly1219@gmail.com>
2021-08-12 11:50:44 +0000

    wrote a new test for RunCpp()

--- tests/services_test.go
+++ tests/services_test.go
@@ -1,13 +1,18 @@
 package tests
 
 import (
-	"fmt"
+	"io/ioutil"
+	"path/filepath"
 	"testing"
 
 	"github.com/jpoly1219/go-leetcode/pkg"
 )
 
 func TestRunCpp(t *testing.T) {
-	output, err := RunCpp(input, pathUserfiles)
-	if 
+	pathUserfiles := filepath.Join(".", "testuserfiles")
+	input, _ := ioutil.ReadFile(filepath.Join(pathUserfiles, "test.cpp"))
+	output, _ := pkg.RunCpp(input, pathUserfiles)
+	if output != "running test.cpp\n" {
+		t.Errorf("expected 'running test.cpp'")
+	}
 }
