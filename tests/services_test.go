6c7e8586c49513f736dbb19fc8a98a75bc2b31c0
Jacob Kim <jpoly1219@gmail.com>
2021-08-13 14:43:01 +0000

    added table-driven tests to TestRunJava()

--- tests/services_test.go
+++ tests/services_test.go
@@ -35,11 +35,28 @@ func TestRunCpp(t *testing.T) {
 }
 
 func TestRunJava(t *testing.T) {
+	type javaTest struct {
+		input                  []byte
+		dirUserfiles, expected string
+	}
+
 	pathUserfiles := filepath.Join(".", "testuserfiles")
-	input, _ := ioutil.ReadFile(filepath.Join(pathUserfiles, "test.java"))
-	output, _ := pkg.RunJava(input, pathUserfiles)
-	if output != "running test.java\n" {
-		t.Errorf("expected 'running test.java'")
+	input1, _ := ioutil.ReadFile(filepath.Join(pathUserfiles, "test1.java"))
+	input2, _ := ioutil.ReadFile(filepath.Join(pathUserfiles, "test2.java"))
+	input3, _ := ioutil.ReadFile(filepath.Join(pathUserfiles, "test3.java"))
+	input4, _ := ioutil.ReadFile(filepath.Join(pathUserfiles, "test4.java"))
+	javaTests := []javaTest{
+		{input1, pathUserfiles, "running test.java\n"},
+		{input2, pathUserfiles, "The string is a palindrome.\n"},
+		{input3, pathUserfiles, "5 6 11 12 13 \n"},
+		{input4, pathUserfiles, "The Shortest Superstring is gctaagttcatgcatc\n"},
+	}
+
+	for _, test := range javaTests {
+		output, _ := pkg.RunJava(test.input, test.dirUserfiles)
+		if output != test.expected {
+			t.Errorf("Output %q not equal to expected %q", output, test.expected)
+		}
 	}
 }
 
