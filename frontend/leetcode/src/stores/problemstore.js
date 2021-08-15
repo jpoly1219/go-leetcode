773567ef72a834628a863da68f919577d81e896d
Jacob Kim <jpoly1219@gmail.com>
2021-08-15 01:50:03 +0000

    fetches the backend api for problem sets

--- frontend/leetcode/src/stores/problemstore.js
+++ frontend/leetcode/src/stores/problemstore.js
@@ -1,1 +1,20 @@
-// pull problems as JSON from the backend api
+import { writable } from "svelte/store"
+
+export const problems = writable([])
+
+const fetchProblem = async () => {
+    const url = "http://jpoly1219devbox.xyz:8090/problemsets"
+    const res = await fetch(url)
+    const data = await res.json()
+    const loadedProblem = data.map((data, index) => {
+        return {
+            num: index + 1,
+            title: data.title,
+            difficulty: data.difficulty,
+            description: data.description,
+        }
+    })
+    problems.set(loadedProblem)
+}
+
+fetchProblem()
