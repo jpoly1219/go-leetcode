303e112d99246bf5f1e553b1e26213190fc0970d
Jacob Kim <jpoly1219@gmail.com>
2021-08-15 01:49:28 +0000

    problemset page now renders problems that are stored inside problemstore.js

--- frontend/leetcode/src/routes/problemset.svelte
+++ frontend/leetcode/src/routes/problemset.svelte
@@ -1,5 +1,13 @@
+<script>
+    import { problems } from "../stores/problemstore"
+    console.log($problems)
+</script>
+
 <svelte:head>
     <title>Problems - go-leetcode</title>
 </svelte:head>
 
 <h1>Problem Sets</h1>
+{#each $problems as problem}
+    <p>{problem.title}</p>
+{/each}
