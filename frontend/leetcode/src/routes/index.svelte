efcd6f4444cfd8fb250ad7c5d79fbfd188e9fe3d
Jacob Kim <jpoly1219@gmail.com>
2021-08-15 02:20:20 +0000

    added simple styling

--- frontend/leetcode/src/routes/index.svelte
+++ frontend/leetcode/src/routes/index.svelte
@@ -1,5 +1,4 @@
 <script>
-    
     console.log("reached index page")
 </script>
 
@@ -7,5 +6,5 @@
     <title>go-leetcode</title>
 </svelte:head>
 
-<h1>Welcome to go-leetcode!</h1>
+<h1 class="text-4xl text-center my-8">Welcome to go-leetcode!</h1>
 <p>Here you can practice your programming skills with our curated questions, just like the OG Leetcode app.</p>
