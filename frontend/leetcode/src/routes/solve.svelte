80e70cb6fed8f44a857e7e5aa51117720b60f369
Jacob Kim <jpoly1219@gmail.com>
2021-08-20 13:12:53 +0000

    styled markdown using typography

--- frontend/leetcode/src/routes/solve.svelte
+++ frontend/leetcode/src/routes/solve.svelte
@@ -7,16 +7,18 @@
     <title>Problems - go-leetcode</title>
 </svelte:head>
 
-<h1 class="text-4xl text-center my-8">Problem Sets</h1>
-<div class="grid grid-cols-2 gap-6">
-    <div class="border border-gray-300 rounded-lg">
-        {#each $problems as problem}
-        <p class="text-lg my-2">{problem.title}</p>
-        <p class="text-sm font-extralight text-gray-500 my-2">{problem.difficulty}</p>
-        <p class="text-sm font-light">{@html snarkdown(problem.description)}</p>
-        {/each}
-    </div>
-    <div class="border border-gray-300 rounded-lg">
-        <p>Your code here!</p>
+<div class="h-screen">
+    <h1 class="text-4xl text-center my-8">Problem Sets</h1>
+    <div class="grid grid-cols-2 gap-6">
+        <div class="prose border border-gray-300 rounded-lg max-w-max overflow-auto">
+            {#each $problems as problem}
+            <p class="">{problem.title}</p>
+            <p class="">{problem.difficulty}</p>
+            <p class="">{@html snarkdown(problem.description)}</p>
+            {/each}
+        </div>
+        <div class="border border-gray-300 rounded-lg">
+            <p>Your code here!</p>
+        </div>
     </div>
 </div>
