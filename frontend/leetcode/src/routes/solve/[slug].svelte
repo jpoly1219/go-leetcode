a25473dae5b9333a4878ed8adc2f9359e705782a
Jacob Kim <jpoly1219@gmail.com>
2021-08-21 08:35:45 +0000

    removed unnecessary imports, fixed typo on the h1 tag

--- frontend/leetcode/src/routes/solve/[slug].svelte
+++ frontend/leetcode/src/routes/solve/[slug].svelte
@@ -1,13 +1,7 @@
-<script>
-
-import { problems } from "src/stores/problemstore";
-import { loop_guard } from "svelte/internal";
-
-</script>
 <script context="module">
     export async function load({page}) {
         const slug = page.params.slug
-        const url = `http://jpoly1219devbox.xyz:8090/${slug}`
+        const url = `http://jpoly1219devbox.xyz:8090/solve/${slug}`
         const res = await fetch(url)
         const problem = await res.json()
         return {props: {problem}}
@@ -18,4 +12,4 @@ import { loop_guard } from "svelte/internal";
     export let problem
 </script>
 
-<h1>{problems.title}</h1>
+<h1>{problem.title}</h1>
