e12e6520dea249489f291cfd4fd77e5b931c2e0a
Jacob Kim <jpoly1219@gmail.com>
2021-08-21 06:14:29 +0000

    created a dynamic route component

---
+++ frontend/leetcode/src/routes/solve/[slug].svelte
@@ -0,0 +1,21 @@
+<script>
+
+import { problems } from "src/stores/problemstore";
+import { loop_guard } from "svelte/internal";
+
+</script>
+<script context="module">
+    export async function load({page}) {
+        const slug = page.params.slug
+        const url = `http://jpoly1219devbox.xyz:8090/${slug}`
+        const res = await fetch(url)
+        const problem = await res.json()
+        return {props: {problem}}
+    }
+</script>
+
+<script>
+    export let problem
+</script>
+
+<h1>{problems.title}</h1>
