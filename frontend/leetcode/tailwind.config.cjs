7cc05c1371beb278d2891e696fb599f42ef47797
Jacob Kim <jpoly1219@gmail.com>
2021-08-22 06:07:00 +0000

    created custom commands for grid span and rows

--- frontend/leetcode/tailwind.config.cjs
+++ frontend/leetcode/tailwind.config.cjs
@@ -1,5 +1,15 @@
 module.exports = {
   mode: 'jit',
   purge: ['./src/**/*.svelte'],
-  plugins: [require('@tailwindcss/typography')]
+  plugins: [require('@tailwindcss/typography')],
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        '12': 'repeat(12, minmax(0, 1fr))',
+      },
+      gridRow: {
+        'span-11': 'span 11 / span 11',
+      }
+    }
+  }
 }
