ef51121a17eecc8e0957edf5da23c0d221f5c5fb
Jacob Kim <jpoly1219@gmail.com>
2021-08-22 07:28:31 +0000

    fixed styling for the left side of the bottom bar

--- frontend/leetcode/src/routes/solve/[slug].svelte
+++ frontend/leetcode/src/routes/solve/[slug].svelte
@@ -28,20 +28,22 @@
             <p>Your code here!</p>
         </div>
     </div>
-    <div class="row-span-1 border border-gray-300 rounded-lg">
-        <div class="grid grid-cols-2">
-            <div class="flex flex-row">
-                <button>Problems</button>
-                <button>Pick One</button>
-                <button>Prev</button>
-                <p>1/1977</p>
-                <button>Next</button>
+    <div class="row-span-1 grid grid-cols-2 content-center border border-gray-300 rounded-lg">
+        <div class="flex flex-row">
+            <div class="flex-1 flex">
+                <button class="border border-gray-300 rounded-lg px-3 py-2">Problems</button>
             </div>
-            <div class="flex flex-row">
-                <button>Console</button>
-                <button>Run Code</button>
-                <button>Submit</button>
+            <button class="border border-gray-300 rounded-lg px-3 py-2">Pick One</button>
+            <button class="border border-gray-300 rounded-lg px-3 py-2 mx-4">Prev</button>
+            <div class="mx-2 flex">
+                <p class="self-center">1/1977</p>
             </div>
+            <button class="border border-gray-300 rounded-lg px-3 py-2 mx-4">Next</button>
+        </div>
+        <div class="flex flex-row">
+            <button>Console</button>
+            <button>Run Code</button>
+            <button>Submit</button>
         </div>
     </div>
 </div>
