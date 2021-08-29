a8265424a4efad303616436262fdfec4fbadb32a
Jacob Kim <jpoly1219@gmail.com>
2021-08-29 14:14:53 +0000

    now saves result to json when all cases pass as well

--- containers/cpp/files/test1.cpp
+++ containers/cpp/files/test1.cpp
@@ -49,7 +49,10 @@ int main() {
             o << output << endl;
         }
         else {
-            cout << "OK" << endl;
+            json output = {
+                {"result", "OK"}
+            };
+            o << output << endl;
         }
     }
     i.close();
