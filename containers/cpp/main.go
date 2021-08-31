ab90ef5404cbd191fb18e63212a882c32285b572
Jacob Kim <jpoly1219@gmail.com>
2021-08-31 14:43:01 +0000

    divided testing and send/receiving output by splitting RunCpp() and RunTest()

--- containers/cpp/main.go
+++ containers/cpp/main.go
@@ -2,20 +2,37 @@ package main
 
 import (
 	"C"
-	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
+import "os/exec"
 
-func RunCpp(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("running cpp")
+func RunCpp() {
+	// receive request with user code inside
+	// insert user code into file at a certain place within the template file
+	// compile and run
+	// read result from result.json
+	// save to submissions database. (columns = username, question number, language, code, runtime, result, output)
+	// send results and output back as JSON
+}
+
+func RunTest(w http.ResponseWriter, r *http.Request) {
+	// insert user code
+	
+	// main_test.go will do the testing of user code and generate an output
+	// this will be sent back to the user
+	output, err := exec.Command("go", "test").Output()
+	if err != nil {
+		panic(err.Error())
+	}
+	w.Write([]byte(output))
 }
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/run-cpp", RunCpp)
+	r.HandleFunc("/run-cpp", RunTest)
 
 	log.Fatal(http.ListenAndServe(":8090", r))
 }
