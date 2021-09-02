5ec46aa247b5b4545bc619ee6525107a29ebabc4
Jacob Kim <jpoly1219@gmail.com>
2021-09-02 13:56:34 +0000

    moved template.cpp to root directory

---
+++ containers/cpp/template.cpp
@@ -0,0 +1,57 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include "json.hpp"
+
+using namespace std;
+using json = nlohmann::json;
+
+// insert Solution class from the backend using Go's file write functions
+class Solution;
+
+int main() {
+    Solution sol;
+
+    // add testcases from the backend using Go's file write functions
+    // tc.vecNums = ...
+    // tc.vecTarget = ...
+    // tc.vecExpected = ...
+    ifstream i("testcase.json");
+    json j;
+    i >> j;
+
+    ofstream o("result.json", ios::trunc);
+
+    vector<vector<int>> vecNums = j["input"]["nums"];
+    vector<int> vecTargets = j["input"]["target"];
+    vector<vector<int>> vecExpected = j["expected"];
+
+    bool isOk = true;
+    // test
+    for (int i = 0; i < vecNums.size(); i++) {
+        vector<int> vecSolution = sol.twoSum(vecNums.at(i), vecTargets.at(i));
+        if (vecSolution != vecExpected.at(i)) {
+            json output = {
+                {"result", "wrong"},
+                {"input", vecNums.at(i)},
+                {"expected", vecExpected.at(i)},
+                {"output", vecSolution}
+            };
+            o << output << endl;
+            isOk = false;
+            break;
+        }
+        else {
+            cout << "OK" << endl;
+        }
+    }
+    if (isOk) {
+        json output = {
+            {"result", "OK"}
+        };
+        o << output << endl;
+    }
+    i.close();
+    o.close();
+}
+
