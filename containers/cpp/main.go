addc1a612b0c5dce4f89023009741f6e0910040c
Jacob Kim <jpoly1219@gmail.com>
2021-09-03 02:37:02 +0000

    RunCpp() compiles and runs the cpp code, then reads in result.json

--- containers/cpp/main.go
+++ containers/cpp/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os/exec"
 
 	"encoding/json"
 	"fmt"
@@ -12,10 +13,38 @@ import (
 )
 
 func RunCpp() {
-	// receive request with user code inside
-	// insert user code into file at a certain place within the template file
+	// receive request with user code inside (RunTest)
+	// insert user code into file at a certain place within the template file (RunTest)
 	// compile and run
+	cmd := exec.Command("g++", "template.cpp", "-o", "template.out")
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	out, err := exec.Command("template.out").Output()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(out))
+
 	// read result from result.json
+	type resultFile struct {
+		Result   string `json:"result"`
+		Input    string `json:"input"`
+		Expected string `json:"expected"`
+		Output   string `json:"output"`
+	}
+	var result resultFile
+
+	f, err := os.ReadFile("result.json")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	json.Unmarshal(f, &result)
 	// save to submissions database. (columns = username, question number, language, code, runtime, result, output)
 	// send results and output back as JSON
 }
