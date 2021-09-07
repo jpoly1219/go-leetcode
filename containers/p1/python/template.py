47e2a022c1deae1cfa471409d6f633e1145e6aeb
Jacob Kim <jpoly1219@gmail.com>
2021-09-07 14:46:59 +0000

    created a basic testing structure

--- containers/p1/python/template.py
+++ containers/p1/python/template.py
@@ -1,6 +1,25 @@
-# insert Solution class here
+import json
 
+# insert Solution class here
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        print("Hello world!")
+        pass
 
+with open("testcase.json", "r") as read_file:
+    data = json.load(read_file)
+
+sol = Solution()
+isOk = True
+# for each input pair, check if Solution.twoSum() returns a matching expected answer
+for i in range(len(data["input"]["nums"])):
+    answer = sol.twoSum(data["input"]["num"][i], data["input"]["target"][i])
+    if answer != data["expected"][i]:
+        # write json to file
+        isOk = False
+        break
+
+if isOk:
+    # write json to file
+    print("ok")
+
+print("done")
