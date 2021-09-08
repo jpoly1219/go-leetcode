a7f6fa0ddeea56b2dd6172c8b948b965ea15870f
Jacob Kim <jpoly1219@gmail.com>
2021-09-08 14:01:25 +0000

    updated JSON write logic

--- containers/p1/python/template.py
+++ containers/p1/python/template.py
@@ -1,9 +1,9 @@
 import json
+from typing import List
+
 
 # insert Solution class here
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        pass
+
 
 with open("testcase.json", "r") as read_file:
     data = json.load(read_file)
@@ -12,14 +12,29 @@ sol = Solution()
 isOk = True
 # for each input pair, check if Solution.twoSum() returns a matching expected answer
 for i in range(len(data["input"]["nums"])):
-    answer = sol.twoSum(data["input"]["num"][i], data["input"]["target"][i])
+    answer = sol.twoSum(data["input"]["nums"][i], data["input"]["target"][i])
     if answer != data["expected"][i]:
         # write json to file
+        output = {
+            "result": "wrong",
+            "input": data["input"]["nums"][i],
+            "expected": data["expected"][i],
+            "output": answer
+        }
+        json_object = json.dumps(output, indent=4)
+        with open("result.json", "w") as outfile:
+            outfile.write(json_object)
+        
         isOk = False
         break
 
 if isOk:
     # write json to file
-    print("ok")
+    output = {
+        "result": "OK"
+    }
+    json_object = json.dumps(output, indent=4)
+    with open("result.json", "w") as outfile:
+        outfile.write(json_object)
 
 print("done")
