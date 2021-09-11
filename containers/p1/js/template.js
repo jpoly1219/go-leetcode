a70d4e3f87991ce3315433f6ec6d871f70c74d29
Jacob Kim <jpoly1219@gmail.com>
2021-09-11 03:59:03 +0000

    updated template.js

--- containers/p1/js/template.js
+++ containers/p1/js/template.js
@@ -1,4 +1,46 @@
 const fs = require("fs")
-const testcase = require("../testcase.json")
+const tc = require("../testcase.json")
 
-console.log(testcase)
+// insert Solution class here
+var twoSum = function(nums, target) {
+    const output = []
+    nums.forEach(num => {
+        output.push(num + target)
+    });
+
+    return output
+};
+
+let isOk = true
+for (let i = 0; i < tc.input.nums.length; i++) {
+    const answer = twoSum(tc.input.nums[i], tc.input.target[i])
+    if (answer != tc.expected[i]) {
+        const output = {
+            "result": "wrong",
+            "input": tc.input.nums[i],
+            "expected": tc.expected[i],
+            "output": answer
+        }
+        const data = JSON.stringify(output)
+        fs.writeFile("result.json", data, (err) => {
+            if (err) {
+                throw err
+            }
+        })
+        isOk = false
+    }
+}
+
+if (isOk) {
+    const output = {
+        "result": "OK"
+    }
+    const data = JSON.stringify(output)
+    fs.writeFile("result.json", data, (err) => {
+        if (err) {
+            throw err
+        }
+    })
+}
+
+console.log("done")
