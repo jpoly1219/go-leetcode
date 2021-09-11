180def4f38380869350be811bdefc3bbcf4185ed
Jacob Kim <jpoly1219@gmail.com>
2021-09-11 06:44:28 +0000

    creating template for java. may need to restart this into a gradle project instead

---
+++ containers/p1/java/template.java
@@ -0,0 +1,18 @@
+// insert Solution class here
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[nums.length] output;
+        for (int i = 0; i < nums.length; i++) {
+            output[i] = nums[i] + target;
+        }
+        return output;
+    }
+}
+
+public class Template {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        boolean isOk = true;
+        for (int i = 0; i < )
+    }
+}
