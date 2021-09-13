0f2c8e266e4846f9e24e4be15bf1578218ac652e
Jacob Kim <jpoly1219@gmail.com>
2021-09-13 11:34:55 +0000

    renamed python directory to py

---
+++ containers/p1/py/template.py
@@ -0,0 +1,40 @@
+import json
+from typing import List
+
+
+# insert Solution class here
+
+
+with open("testcase.json", "r") as read_file:
+    data = json.load(read_file)
+
+sol = Solution()
+isOk = True
+# for each input pair, check if Solution.twoSum() returns a matching expected answer
+for i in range(len(data["input"]["nums"])):
+    answer = sol.twoSum(data["input"]["nums"][i], data["input"]["target"][i])
+    if answer != data["expected"][i]:
+        # write json to file
+        output = {
+            "result": "wrong",
+            "input": data["input"]["nums"][i],
+            "expected": data["expected"][i],
+            "output": answer
+        }
+        json_object = json.dumps(output, indent=4)
+        with open("result.json", "w") as outfile:
+            outfile.write(json_object)
+        
+        isOk = False
+        break
+
+if isOk:
+    # write json to file
+    output = {
+        "result": "OK"
+    }
+    json_object = json.dumps(output, indent=4)
+    with open("result.json", "w") as outfile:
+        outfile.write(json_object)
+
+print("done")
