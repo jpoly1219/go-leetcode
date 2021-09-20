0d71215ab5bcc169901d5f62e91f10c6eda9aa53
Jacob Kim <jpoly1219@gmail.com>
2021-09-20 07:47:28 +0000

    added links to sign up and log in pages in the navbar

--- frontend/leetcode/src/components/nav.svelte
+++ frontend/leetcode/src/components/nav.svelte
@@ -1,4 +1,6 @@
 <nav class="flex justify-center w-full">
     <a href="/" class="mx-4 text-lg">Home</a>
     <a href="/problemset" class="mx-4 text-lg">Problems</a>
+    <a href="/signup" class="mx-4 text-lg">Sign Up</a>
+    <a href="/login" class="mx-4 text-lg">Log In</a>
 </nav>
