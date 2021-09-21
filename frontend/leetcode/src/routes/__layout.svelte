d6a86240c0ce58dd0473e2d289c3c4b3d33f9993
Jacob Kim <jpoly1219@gmail.com>
2021-09-21 10:56:36 +0000

    __layout holds the timer that ticks until access token is expired, which then sends a request to silently refresh

--- frontend/leetcode/src/routes/__layout.svelte
+++ frontend/leetcode/src/routes/__layout.svelte
@@ -1,5 +1,39 @@
 <script>
     import Nav from "../components/nav.svelte"
+    import timeToExpire from "../stores/timer"
+
+    async function refresh() {
+		const options = {
+			method: "POST",
+			credentials: "include"
+		}
+
+		try {
+			const res = await fetch("http://jpoly1219devbox.xyz:8090/auth/silentrefresh", options)
+			const accessToken = await res.json()
+			const payloadB64 = accessToken.split(".")[1]
+			timeToExpire.set(JSON.parse(window.atob(payloadB64)).exp)
+		} catch(err) {
+			alert(err)
+		}
+	}
+
+    function refreshTimer() {
+		if ($timeToExpire != "") {
+			var i = Date.now()/1000;
+			var timer = setInterval(() => {
+				console.log($timeToExpire)
+				if (i >= Number($timeToExpire)) {
+                    refresh()
+                    clearInterval(timer)
+				}	
+				console.log(Date.now()/1000)
+				i++
+			}, 1000)
+		}
+	}
+
+	$: $timeToExpire, refreshTimer()
 </script>
 
 <div class="p-8 h-screen">
