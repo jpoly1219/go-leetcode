f1a71f9308510bc4c316963cbe87ce3e4a7c6922
Jacob Kim <jpoly1219@gmail.com>
2021-10-10 06:52:48 +0000

    created tabs component to use within the app

---
+++ frontend/leetcode/src/components/tabs.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { createEventDispatcher } from 'svelte'
+    
+    const dispatch = createEventDispatcher()
+
+    export let tabs
+    export let activeTab
+</script>
+
+<div>
+    <ul>
+        {#each tabs as tab}
+        <li on:click={() => dispatch("tabChange", tab)}>
+            <div>{tab}</div>
+        </li>
+        {/each}
+    </ul>
+</div>
