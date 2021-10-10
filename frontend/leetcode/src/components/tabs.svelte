3325ae3ac800dbd0414935a2b3f200128e102528
Jacob Kim <jpoly1219@gmail.com>
2021-10-10 07:04:05 +0000

    created tabs for description, solution, discussion and submissions

--- frontend/leetcode/src/components/tabs.svelte
+++ frontend/leetcode/src/components/tabs.svelte
@@ -1,16 +1,16 @@
 <script>
     import { createEventDispatcher } from 'svelte'
-    
+
     const dispatch = createEventDispatcher()
 
     export let tabs
     export let activeTab
 </script>
 
-<div>
-    <ul>
+<div class="mb-3">
+    <ul class="flex justify-start p-0 list-none">
         {#each tabs as tab}
-        <li on:click={() => dispatch("tabChange", tab)}>
+        <li on:click={() => dispatch("tabChange", tab)} class="mr-4 text-sm cursor-pointer">
             <div>{tab}</div>
         </li>
         {/each}
