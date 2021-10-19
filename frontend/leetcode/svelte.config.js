121a5cfcce3dacbd7cb5ae9a2a183a0ceb912a43
Jacob Kim <jpoly1219@gmail.com>
2021-10-19 14:36:20 +0000

    updated svelte config

--- frontend/leetcode/svelte.config.js
+++ frontend/leetcode/svelte.config.js
@@ -1,10 +1,17 @@
-@tailwind base;
-@tailwind components;
-@tailwind utilities;/** @type {import('@sveltejs/kit').Config} */
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		vite: {
+			server: {
+				hmr: {
+					host: 'localhost',
+					port: 15000,
+					protocol: 'ws'
+				}
+			}
+		}
 	}
 };
 
