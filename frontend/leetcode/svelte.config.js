161099899d74fb2c62fb64fb94e6a2a345be4557
Jacob Kim <jpoly1219@gmail.com>
2021-10-20 10:45:45 +0000

    fixing config

--- frontend/leetcode/svelte.config.js
+++ frontend/leetcode/svelte.config.js
@@ -1,6 +1,10 @@
+import node from '@sveltejs/adapter-node';
+
 /** @type {import('@sveltejs/kit').Config} */
+
 const config = {
 	kit: {
+		adapter: node(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		vite: {
