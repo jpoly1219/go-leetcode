f495a2214f37ca2e57fc31265da95f00f49e170b
Jacob Kim <jpoly1219@gmail.com>
2021-10-29 01:34:55 +0000

    styling table rows

--- frontend/leetcode/src/routes/solve/[slug].svelte
+++ frontend/leetcode/src/routes/solve/[slug].svelte
@@ -107,11 +107,11 @@
             <p class="text-lg font-bold">Submissions</p>
                 {#if submissionsData}
                 <div class="w-full">
-                    <table class="items-center w-full border-collapse">
+                    <table class="table-fixed items-center w-full border-collapse">
                         <thead>
                             <tr>
-                                <th class="px-4 py-2 bg-gray-300 border border-solid border-gray-400 border-r-0 text-sm text-gray-700 text-left">Result</th>
-                                <th class="px-4 py-2 bg-gray-300 border border-solid border-gray-400 border-l-0 text-sm text-gray-700 text-left">Output</th>
+                                <th class="w-1/5 px-4 py-2 bg-gray-300 border border-solid border-gray-400 border-r-0 text-sm text-gray-700 text-left">Result</th>
+                                <th class="w-4/5 px-4 py-2 bg-gray-300 border border-solid border-gray-400 border-l-0 text-sm text-gray-700 text-left">Output</th>
                             </tr>
                         </thead>
                         {#each submissionsData as submissionsDatum}
@@ -120,7 +120,7 @@
                                 <td class="px-4 py-2 text-sm text-left {submissionsDatum.result === 'OK' ? 'text-green-600' : 'text-red-600'}">
                                     {submissionsDatum.result}
                                 </td>
-                                <td class="break-words">{submissionsDatum.output}</td>
+                                <td class="px-4 py-2 text-sm text-left break-words">{submissionsDatum.output}</td>
                             </tr>
                         </tbody>
                         {/each}
