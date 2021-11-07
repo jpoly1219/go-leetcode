aa00cd82728389f238c21bd482f340b4d1cb52e5
Jacob Kim <jpoly1219@gmail.com>
2021-11-07 14:20:09 +0000

    fixing cookie settings

--- backend/pkg/auth.go
+++ backend/pkg/auth.go
@@ -193,6 +193,8 @@ func SilentRefresh(w http.ResponseWriter, r *http.Request) {
 			Value:    tokenPair.RefreshToken,
 			Domain:   "jpoly1219devbox.xyz",
 			Path:     "/auth/",
+			SameSite: http.SameSiteNoneMode,
+			Secure:   true,
 		}
 		http.SetCookie(w, &cookie)
 		w.Header().Set("Access-Control-Allow-Origin", "http://jpoly1219devbox.xyz:5000")
