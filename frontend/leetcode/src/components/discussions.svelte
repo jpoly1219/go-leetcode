e422d320aca1f6d370bd65f6aaa732d125b57165
Jacob Kim <jpoly1219@gmail.com>
2021-11-09 14:39:59 +0000

    created component to render all discussioncard components

--- frontend/leetcode/src/components/discussions.svelte
+++ frontend/leetcode/src/components/discussions.svelte
@@ -1,8 +1,28 @@
 <script>
+    import Discussioncard from "./discussioncard.svelte"
+    import Singlediscussion from "./singlediscussion.svelte"
+
     // props
     export let slug
+    export let discussions
 
+    // variable to show which component to render
+    let currentComponent
+    let props
+
+    // event listener to change currentView
+    const handleSwitch = (event) => {
+        currentComponent = event.detail.component
+        props = event.detail.props
+    }
 </script>
 
-<p class="text-lg font-bold mb-3">Discussion Board</p>
-<p class="text-lg font-bold mb-3">{slug}</p>
+<p class="text-lg font-bold mb-3">Discussion Board for {slug}</p>
+
+{#if currentComponent === "Singlediscussion"}
+    <Singlediscussion {...props} on:switch={handleSwitch}/>
+{:else}
+    {#each discussions as discussion}
+        <Discussioncard discussion={discussion} on:switch={handleSwitch}/>
+    {/each}
+{/if}
