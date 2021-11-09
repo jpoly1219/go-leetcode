a22c24edb1f0001685963fe58914f25cac0f3e7e
Jacob Kim <jpoly1219@gmail.com>
2021-11-09 14:39:46 +0000

    created component to render each discussion as a card

---
+++ frontend/leetcode/src/components/discussioncard.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+
+    export let discussion
+
+    const dispatch = createEventDispatcher();
+    const switchComponent = () => {
+        dispatch('switch', {
+            component: 'Singlediscussion',
+            props: {
+                title: discussion.title,
+                author: discussion.author,
+                description: discussion.description,
+                created: discussion.created
+            }
+        })
+    }
+</script>
+
+<p on:click={switchComponent}>{discussion.title}</p>
