a74e9f408beaf4d2854509a6de49a774dd701c18
Jacob Kim <jpoly1219@gmail.com>
2021-11-11 14:32:33 +0000

    rendering description with snarkdown

--- frontend/leetcode/src/components/singlediscussion.svelte
+++ frontend/leetcode/src/components/singlediscussion.svelte
@@ -12,12 +12,12 @@
 </script>
 
 <div>
-    <div class="flex flex-row divide-x divide-solid divide-gray-100">
-        <p on:click={switchComponent} class="text-sm px-3 py-2">&gt; Back</p>
-        <p class="font-bold text-lg">{discussion.title}</p>
+    <div class="flex flex-row divide-x divide-solid divide-gray-300">
+        <p on:click={switchComponent} class="text-sm mx-3 my-2">&lt; Back</p>
+        <p class="font-bold text-lg mx-3 my-2">{discussion.title}</p>
     </div>
     <div>
-        <p class="text-sm">{discussion.author}</p>
-        <p class="text-base">{discussion.description}</p>
+        <p class="text-sm mb-2">{discussion.author}</p>
+        <p class="prose max-w-max">{@html snarkdown(discussion.description)}</p>
     </div>
 </div>
