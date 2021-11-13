e07ee47e45349bb8da10c29129360aad83dd2e60
Jacob Kim <jpoly1219@gmail.com>
2021-11-13 06:33:27 +0000

    created comment section with fake data

--- frontend/leetcode/src/components/singlediscussion.svelte
+++ frontend/leetcode/src/components/singlediscussion.svelte
@@ -12,7 +12,7 @@
     }
 </script>
 
-<div>
+<div class="overflow-auto">
     <div class="flex flex-row divide-x divide-solid divide-gray-300">
         <p on:click={switchComponent} class="text-sm mx-3 my-2">&lt; Back</p>
         <p class="font-bold text-lg mx-3 my-2">{discussion.title}</p>
@@ -20,5 +20,24 @@
     <div>
         <p class="text-sm mb-2">{discussion.author}</p>
         <p class="prose max-w-max">{@html snarkdown(discussion.description)}</p>
+    </div>
+    <div class="my-2 border-t-2 border-b-2 border-gray-400">
+        <p class="text-base">Comments:</p>
+    </div>
+    <div class="">
+        <div class="">
+            <div class="flex flex-row">
+                <img 
+                    src="https://img.freepik.com/free-photo/pleasant-looking-serious-man-stands-profile-has-confident-expression-wears-casual-white-t-shirt_273609-16959.jpg?size=626&ext=jpg"
+                    class="rounded-full w-12 h-12 mr-2"
+                >
+                <p class="text-sm mr-2">username</p>
+                <p class="text-sm">time created</p>
+            </div>
+            <div class="flex flex-row">
+                <div class="w-12 mr-2"></div>
+                <p>This is a comment.</p>
+            </div>
+        </div>
     </div>
 </div>
