4767ae2935ff020a10221d16b906ca1b276e0897
Jacob Kim <jpoly1219@gmail.com>
2021-11-13 14:24:45 +0000

    styled each discussion cards

--- frontend/leetcode/src/components/discussioncard.svelte
+++ frontend/leetcode/src/components/discussioncard.svelte
@@ -14,4 +14,12 @@
     }
 </script>
 
-<p on:click={switchComponent}>{discussion.title}</p>
+<div>
+    <div class="flex flex-row items-center py-2">
+        <img src="https://www.catholicsingles.com/wp-content/uploads/2020/06/blog-header-3.png" class="rounded-full w-12 h-12 mr-4">
+        <div class="flex flex-col">
+            <p on:click={switchComponent} class="text-base">{discussion.title}</p>
+            <p class="text-xs text-gray-500">{discussion.author} created at: {discussion.created}</p>
+        </div>
+    </div>
+</div>
