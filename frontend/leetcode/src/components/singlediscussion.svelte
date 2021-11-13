f6c0b7dcdbcc131effd8f79babb828b67c5b09e2
Jacob Kim <jpoly1219@gmail.com>
2021-11-13 14:25:34 +0000

    styled the title bar for each discussion

--- frontend/leetcode/src/components/singlediscussion.svelte
+++ frontend/leetcode/src/components/singlediscussion.svelte
@@ -16,8 +16,8 @@
 </script>
 
 <div class="overflow-auto">
-    <div class="flex flex-row divide-x divide-solid divide-gray-300">
-        <p on:click={switchComponent} class="text-sm mx-3 my-2">&lt; Back</p>
+    <div class="flex flex-row divide-x divide-solid divide-gray-300 items-center">
+        <p on:click={switchComponent} class="text-sm mr-3 my-2 cursor-pointer">&lt; Back</p>
         <p class="font-bold text-lg px-3 my-2">{discussion.title}</p>
     </div>
     <div>
