38341ad0da7989f3490ab9f648be0c7873eb6b0b
Jacob Kim <jpoly1219@gmail.com>
2021-11-21 14:43:46 +0000

    styling page layout

--- frontend/leetcode/src/components/nav.svelte
+++ frontend/leetcode/src/components/nav.svelte
@@ -12,7 +12,7 @@
         }
     })
 </script>
-<nav class="flex justify-start w-full bg-gray-800 px-8 items-center">
+<nav class="flex justify-start w-full bg-gray-800 py-8 items-center">
     <p class="mx-4 px-3 py-2 rounded-md text-lg text-white font-extrabold">go-leetcode</p>
     <a href="/" class="mx-4 px-3 py-2 rounded-md text-lg text-white font-medium">Home</a>
     <a href="/problemset" class="mx-4 px-3 py-2 rounded-md text-lg text-white font-medium">Problems</a>
