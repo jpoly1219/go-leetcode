d00b3b1dbed5d7c0aa12379b003d7c2ba444cce7
Jacob Kim <jpoly1219@gmail.com>
2021-11-22 13:29:40 +0000

    styling page layout

--- frontend/leetcode/src/components/nav.svelte
+++ frontend/leetcode/src/components/nav.svelte
@@ -11,15 +11,17 @@
             goto("/")
         }
     })
+
+    let currentPage = "home"
 </script>
 <nav class="flex justify-start w-full bg-gray-800 py-8 items-center">
     <p class="mx-4 px-3 py-2 rounded-md text-lg text-white font-extrabold">go-leetcode</p>
-    <a href="/" class="mx-4 px-3 py-2 rounded-md text-lg text-white font-medium">Home</a>
-    <a href="/problemset" class="mx-4 px-3 py-2 rounded-md text-lg text-white font-medium">Problems</a>
+    <a href="/" on:click={() => {currentPage = "home"}} class="mx-4 px-3 py-2 rounded-md text-lg text-white font-medium {currentPage === "home" ? "bg-gray-900" : "hover:bg-gray-700"}">Home</a>
+    <a href="/problemset" on:click={() => {currentPage = "problems"}} class="mx-4 px-3 py-2 rounded-md text-lg text-white font-medium">Problems</a>
     {#if $accessTokenStore != ""}
-        <a href="#" class="mx-4 text-lg text-white">Welcome, {username}</a>
+        <a href="#" on:click={() => {currentPage = "profile"}} class="mx-4 text-lg text-white">Welcome, {username}</a>
     {:else}
-        <a href="/signup" class="mx-4 px-3 py-2 rounded-md text-lg text-white font-medium">Sign Up</a>
-        <a href="/login" class="mx-4 px-3 py-2 rounded-md text-lg text-white font-medium">Log In</a>
+        <a href="/signup" on:click={() => {currentPage = "signup"}} class="mx-4 px-3 py-2 rounded-md text-lg text-white font-medium">Sign Up</a>
+        <a href="/login" on:click={() => {currentPage = "login"}} class="mx-4 px-3 py-2 rounded-md text-lg text-white font-medium">Log In</a>
     {/if}
 </nav>
