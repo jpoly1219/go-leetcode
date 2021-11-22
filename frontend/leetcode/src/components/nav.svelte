9cf45344163b7fb2e16711795eb37afc9d65d188
Jacob Kim <jpoly1219@gmail.com>
2021-11-22 14:09:43 +0000

    styling page layout

--- frontend/leetcode/src/components/nav.svelte
+++ frontend/leetcode/src/components/nav.svelte
@@ -14,14 +14,16 @@
 
     let currentPage = "home"
 </script>
-<nav class="flex justify-start w-full bg-gray-800 py-8 items-center">
+<nav class="flex justify-start w-full bg-gray-800 py-2 items-center sticky top-0">
     <p class="mx-4 px-3 py-2 rounded-md text-lg text-white font-extrabold">go-leetcode</p>
     <a href="/" on:click={() => {currentPage = "home"}} class="mx-4 px-3 py-2 rounded-md text-lg text-white font-medium {currentPage === "home" ? "bg-gray-900" : "hover:bg-gray-700"}">Home</a>
     <a href="/problemset" on:click={() => {currentPage = "problems"}} class="mx-4 px-3 py-2 rounded-md text-lg text-white font-medium {currentPage === "problems" ? "bg-gray-900" : "hover:bg-gray-700"}">Problems</a>
     {#if $accessTokenStore != ""}
         <a href="#" on:click={() => {currentPage = "profile"}} class="mx-4 text-lg text-white {currentPage === "profile" ? "bg-gray-900" : "hover:bg-gray-700"}">Welcome, {username}</a>
     {:else}
+    <div class="absolute right-0">
         <a href="/signup" on:click={() => {currentPage = "signup"}} class="mx-4 px-3 py-2 rounded-md text-lg text-white font-medium {currentPage === "signup" ? "bg-gray-900" : "hover:bg-gray-700"}">Sign Up</a>
         <a href="/login" on:click={() => {currentPage = "login"}} class="mx-4 px-3 py-2 rounded-md text-lg text-white font-medium {currentPage === "login" ? "bg-gray-900" : "hover:bg-gray-700"}">Log In</a>
+    </div>
     {/if}
 </nav>
