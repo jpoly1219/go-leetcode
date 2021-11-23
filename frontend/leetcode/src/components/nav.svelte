2320b0e10bb6aa02e0d9782eaf75041783ed2090
Jacob Kim <jpoly1219@gmail.com>
2021-11-23 13:58:10 +0000

    fixed styling navbar

--- frontend/leetcode/src/components/nav.svelte
+++ frontend/leetcode/src/components/nav.svelte
@@ -14,8 +14,8 @@
 
     let currentPage = "home"
 </script>
-<nav class="flex justify-start w-full bg-gray-800 py-2 items-center sticky top-0">
-    <p class="mx-4 px-3 py-2 rounded-md text-lg text-white font-extrabold">go-leetcode</p>
+<nav class="flex justify-start w-full bg-gray-800 py-2 items-center fixed">
+    <p class="mx-4 px-3 py-2 text-lg text-white font-extrabold">go-leetcode</p>
     <a href="/" on:click={() => {currentPage = "home"}} class="mx-4 px-3 py-2 rounded-md text-lg text-white font-medium {currentPage === "home" ? "bg-gray-900" : "hover:bg-gray-700"}">Home</a>
     <a href="/problemset" on:click={() => {currentPage = "problems"}} class="mx-4 px-3 py-2 rounded-md text-lg text-white font-medium {currentPage === "problems" ? "bg-gray-900" : "hover:bg-gray-700"}">Problems</a>
     {#if $accessTokenStore != ""}
