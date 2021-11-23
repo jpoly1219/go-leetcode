0fbb29bae8be5d0d3442f983363c5add0684ebdf
Jacob Kim <jpoly1219@gmail.com>
2021-11-23 14:25:08 +0000

    styling index page

--- frontend/leetcode/src/routes/index.svelte
+++ frontend/leetcode/src/routes/index.svelte
@@ -6,5 +6,17 @@
     <title>go-leetcode</title>
 </svelte:head>
 
-<h1 class="text-4xl text-center my-8">Welcome to go-leetcode!</h1>
-<p>Here you can practice your programming skills with our curated questions, just like the OG Leetcode app.</p>
+<div class="flex flex-row items-center">
+    <div class="w=2/5 m-10">
+        <h1 class="text-4xl text-center my-14">Welcome to go-leetcode!</h1>
+        <p class="text-lg text-center">
+            Here you can practice your programming skills with our curated questions, just like the OG Leetcode app.
+            All problems are sourced directly from Leetcode.
+        </p>
+    </div>
+    <div class="w-3/5 m-10">
+        <img 
+            src="https://elements-cover-images-0.imgix.net/790945c8-90f0-4ac9-b551-06fa26d4f597?auto=compress%2Cformat&fit=max&w=1170&s=416d544ceb7bab026f982e86952f31ad"
+        >
+    </div>
+</div>
