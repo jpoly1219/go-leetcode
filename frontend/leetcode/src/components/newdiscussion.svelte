22d230882589116dcb85884a3a8f6ee50e355787
Jacob Kim <jpoly1219@gmail.com>
2021-11-29 14:26:52 +0000

    created a new component for creating new discussions

---
+++ frontend/leetcode/src/components/newdiscussion.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher();
+    const switchComponent = () => {
+        dispatch('switch', {
+            component: 'Newdiscussion'
+        })
+    }
+
+    
+</script>
+
+<p>create new discussion here</p>
