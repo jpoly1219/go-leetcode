e431ad50f9d209fa18da4335d77ec3e9d97df19d
Jacob Kim <jpoly1219@gmail.com>
2021-11-29 14:27:11 +0000

    new button loads in the Newdiscussion component

--- frontend/leetcode/src/components/discussions.svelte
+++ frontend/leetcode/src/components/discussions.svelte
@@ -1,6 +1,7 @@
 <script>
     import Discussioncard from "./discussioncard.svelte"
     import Singlediscussion from "./singlediscussion.svelte"
+    import Newdiscussion from "./newdiscussion.svelte";
 
     // props
     export let slug
@@ -20,11 +21,13 @@
 <div class="container w-full">
     {#if currentComponent === "Singlediscussion"}
         <Singlediscussion discussion={props.discussion} on:switch={handleSwitch}/>
+    {:else if currentComponent === "Newdiscussion"}
+        <Newdiscussion />
     {:else}
         <div class="flex flex-row items-center relative">
             <p class="text-lg font-bold mb-3">Discussion Board for {slug}</p>
             <a href="#">
-                <button class="absolute right-0 rounded-md bg-gray-800 px-2 py-1 text-sm text-white">New +</button>
+                <button on:click={currentComponent = "Newdiscussion"} class="absolute right-0 rounded-md bg-gray-800 px-2 py-1 text-sm text-white">New +</button>
             </a>
         </div>
         <div class="divide-y divide-solid divide-gray-300">
