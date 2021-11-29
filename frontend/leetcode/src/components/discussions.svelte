db1ccde0808b974cd645119095dbaae3decb5aa7
Jacob Kim <jpoly1219@gmail.com>
2021-11-29 14:17:13 +0000

    adding create new discussion button

--- frontend/leetcode/src/components/discussions.svelte
+++ frontend/leetcode/src/components/discussions.svelte
@@ -17,13 +17,20 @@
     }
 </script>
 
-{#if currentComponent === "Singlediscussion"}
-    <Singlediscussion discussion={props.discussion} on:switch={handleSwitch}/>
-{:else}
-    <p class="text-lg font-bold mb-3">Discussion Board for {slug}</p>
-    <div class="divide-y divide-solid divide-gray-300">
-    {#each discussions as discussion}
-        <Discussioncard discussion={discussion} on:switch={handleSwitch}/>
-    {/each}
-    </div>
-{/if}
+<div class="container w-full">
+    {#if currentComponent === "Singlediscussion"}
+        <Singlediscussion discussion={props.discussion} on:switch={handleSwitch}/>
+    {:else}
+        <div class="flex flex-row items-center relative">
+            <p class="text-lg font-bold mb-3">Discussion Board for {slug}</p>
+            <a href="#">
+                <button class="absolute right-0 rounded-md bg-gray-800 px-2 py-1 text-sm text-white">New +</button>
+            </a>
+        </div>
+        <div class="divide-y divide-solid divide-gray-300">
+        {#each discussions as discussion}
+            <Discussioncard discussion={discussion} on:switch={handleSwitch}/>
+        {/each}
+        </div>
+    {/if}
+</div>
