90026af3e48abb576e4bdb8c33a1e5f290e12b47
Jacob Kim <jpoly1219@gmail.com>
2021-11-29 14:42:41 +0000

    creating html for newdiscussion

--- frontend/leetcode/src/components/newdiscussion.svelte
+++ frontend/leetcode/src/components/newdiscussion.svelte
@@ -4,11 +4,26 @@
     const dispatch = createEventDispatcher();
     const switchComponent = () => {
         dispatch('switch', {
-            component: 'Newdiscussion'
+            component: 'Discussioncard'
         })
     }
 
-    
+    let title = ""
+    let description = ""
+
+    const postDiscussion = () => {
+
+    }
 </script>
 
-<p>create new discussion here</p>
+<div class="overflow-auto">
+    <div class="flex flex-row divide-x divide-solid divide-gray-300 items-center">
+        <p on:click={switchComponent} class="text-sm mr-3 my-2 cursor-pointer">&lt; Back</p>
+        <p class="font-bold text-lg px-3 my-2">Create New Discussion</p>
+    </div>
+    <div class="mb-5">
+        <textarea bind:value={title} class="w-full h-8 border border-gray-200 rounded" placeholder="Title"></textarea>
+        <textarea bind:value={description} class="w-full h-150 border border-gray-200 rounded" placeholder="Description (Markdown is supported)"></textarea>
+        <span on:click={postDiscussion} class="bg-gray-600 px-2 py-1 mt-3 text-sm text-white cursor-pointer rounded">Post</span>
+    </div>
+</div>
