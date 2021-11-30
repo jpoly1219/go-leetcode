c56da0918b85be1dda4d318db753f11e8bea1d40
Jacob Kim <jpoly1219@gmail.com>
2021-11-30 14:37:45 +0000

    styling newdiscussion

--- frontend/leetcode/src/components/newdiscussion.svelte
+++ frontend/leetcode/src/components/newdiscussion.svelte
@@ -22,8 +22,8 @@
         <p class="font-bold text-lg px-3 my-2">Create New Discussion</p>
     </div>
     <div class="mb-5">
-        <textarea bind:value={title} class="w-full h-7 border border-gray-200 rounded py-2 mb-5" placeholder="Title"></textarea>
-        <textarea bind:value={description} class="w-full h-96 border border-gray-200 rounded py-2 mb-5" placeholder="Description (Markdown is supported)"></textarea>
+        <textarea bind:value={title} class="w-full h-9 border border-gray-200 rounded p-1 mb-5" placeholder="Title"></textarea>
+        <textarea bind:value={description} class="w-full h-96 border border-gray-200 rounded p-1 mb-5" placeholder="Description (Markdown is supported)"></textarea>
         <span on:click={postDiscussion} class="bg-gray-600 px-2 py-1 mt-3 text-sm text-white cursor-pointer rounded">Post</span>
     </div>
 </div>
