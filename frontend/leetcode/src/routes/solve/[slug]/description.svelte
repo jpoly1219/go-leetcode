159c448ce43cfa9ae280975022967fd79263d7b5
Jacob Kim <jpoly1219@gmail.com>
2021-12-07 14:21:43 +0000

    created description page for [slug] page

---
+++ frontend/leetcode/src/routes/solve/[slug]/description.svelte
@@ -0,0 +1,10 @@
+<script>
+
+</script>
+
+<div>
+    <p class="text-lg font-bold mb-3">{problem.title}</p>
+    <p class="text-sm text-green-600 font-light">{problem.difficulty}</p>
+    <hr class="my-4">
+    <p class="prose max-w-max">{@html snarkdown(problem.description)}</p>
+</div>
