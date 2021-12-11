4003157d7fdb33179eba932c251fe060cc65e139
Jacob Kim <jpoly1219@gmail.com>
2021-12-11 04:31:24 +0000

    solution.svelte calls the backend for solution markdown

--- frontend/leetcode/src/routes/solve/[slug]/solution.svelte
+++ frontend/leetcode/src/routes/solve/[slug]/solution.svelte
@@ -1,9 +1,33 @@
-<script>
+<script context="module">
+    import snarkdown from "snarkdown"
 
+    export async function load({page}) {
+        const fullPath = page.path
+        const slugArray = fullPath.split("/")
+        const slug = slugArray[2]
+
+        const url = `http://jpoly1219devbox.xyz:8090/solutions/${slug}`
+
+        const options1 = {
+            method: "GET"
+        }
+
+        try {
+            const res = await fetch(url, options1)
+            const solution = await res.json()
+            return {props: {solution}}
+        } catch(err) {
+            console.log(err)
+        }
+    }
+</script>
+
+<script>
+    export let solution
 </script>
 
 <div>
     <p class="text-lg font-bold mb-3">Solution</p>
     <hr class="my-4">
-    <p class="prose max-w-max">{@html snarkdown(solutions.solution)}</p>
+    <p class="prose max-w-max">{@html snarkdown(solution.solution)}</p>
 </div>
