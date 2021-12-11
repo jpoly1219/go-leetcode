1eadbb3d17b752ba1207ef94f6e6cd144472ef19
Jacob Kim <jpoly1219@gmail.com>
2021-12-11 07:17:42 +0000

    submissions are loaded in SSR

--- frontend/leetcode/src/routes/solve/[slug]/submissions.svelte
+++ frontend/leetcode/src/routes/solve/[slug]/submissions.svelte
@@ -1,10 +1,47 @@
-<script>
+<script context="module">
+    export async function load({page}) {
+        const fullPath = page.path
+        const slugArray = fullPath.split("/")
+        const slug = slugArray[2]
 
+        const url = `http://jpoly1219devbox.xyz:8090/submissions/`
+
+        const options = {
+            method: "POST",
+            body: JSON.stringify({username: username, slug: slug})
+        }
+
+        try {
+            const res = await fetch(url, options)
+            const data = await res.json()
+            const loadedSubmission = data.map((data, index) => {
+                return {
+                    num: index + 1,
+                    title: data.title,
+                    slug: data.slug,
+                    lang: data.lang,
+                    code: data.code,
+                    result: data.result,
+                    input: data.input,
+                    expected: data.expected,
+                    output: data.output
+                }
+            })
+
+            return {props: {submissions: loadedSubmission}}
+        } catch(err) {
+            console.log(err)
+        }
+    }
+</script>
+
+<script>
+    export let submissions
 </script>
 
 <div>
     <p class="text-lg font-bold mb-3">Submissions</p>
-    {#if submissionsData}
+    {#if submissions}
         <div class="w-full">
             <table class="table-fixed items-center w-full border-collapse">
                 <thead>
@@ -13,13 +50,13 @@
                         <th class="w-4/5 px-4 py-2 bg-gray-200 border border-solid border-gray-100 border-l-0 text-sm text-gray-700 text-left">Output</th>
                     </tr>
                 </thead>
-                {#each submissionsData as submissionsDatum}
+                {#each submissions as submission}
                     <tbody>
                         <tr>
-                            <td class="px-4 py-2 text-sm text-left {submissionsDatum.result === 'OK' ? 'text-green-600' : 'text-red-600'}">
-                                {submissionsDatum.result}
+                            <td class="px-4 py-2 text-sm text-left {submission.result === 'OK' ? 'text-green-600' : 'text-red-600'}">
+                                {submission.result}
                             </td>
-                            <td class="px-4 py-2 text-sm text-left break-words">{submissionsDatum.output}</td>
+                            <td class="px-4 py-2 text-sm text-left break-words">{submission.output}</td>
                         </tr>
                     </tbody>
                 {/each}
