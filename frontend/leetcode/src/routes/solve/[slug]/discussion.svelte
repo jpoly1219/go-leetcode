db898e0a4e35a1cce8eec8ef804461f83a7a54d9
Jacob Kim <jpoly1219@gmail.com>
2021-12-11 06:57:58 +0000

    finished discussion page

--- frontend/leetcode/src/routes/solve/[slug]/discussion.svelte
+++ frontend/leetcode/src/routes/solve/[slug]/discussion.svelte
@@ -1,7 +1,35 @@
 <script>
+import Discussions from "src/components/discussions.svelte";
+import { load } from "./solution.svelte";
 
+</script>
+<script context="module">
+    export async function load({page}) {
+        const fullPath = page.path
+        const slugArray = fullPath.split("/")
+        const slug = slugArray[2]
+
+        const url = `http://jpoly1219devbox.xyz:8090/discussions/${slug}`
+
+        const options = {
+            method: "GET"
+        }
+
+        try {
+            const res = await fetch(url, options)
+            const discussions = await res.json()
+            return {props: {slug, discussions}}
+        } catch(err) {
+            console.log(err)
+        }
+    }
+</script>
+
+<script>
+    export let slug
+    export let discussions
 </script>
 
 <div>
-    
+    <Discussions slug={slug} discussions={discussions}/>
 </div>
