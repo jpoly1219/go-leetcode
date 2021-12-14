adf6c0940d9b2aee962745f5d3292032f53b1036
Jacob Kim <jpoly1219@gmail.com>
2021-12-14 10:07:19 +0000

    wrapped fetch in try catch

--- frontend/leetcode/src/routes/problemset.svelte
+++ frontend/leetcode/src/routes/problemset.svelte
@@ -1,20 +1,25 @@
 <script context="module">
     export async function load() {
         const url = "http://jpoly1219devbox.xyz:8090/problemsets"
-        const res = await fetch(url)
-        const data = await res.json()
-        console.log(data, typeof(data))
-        const loadedProblem = data.map((data, index) => {
-            return {
-                num: index + 1,
-                title: data.title,
-                slug: data.slug,
-                difficulty: data.difficulty,
-                description: data.description,
-                created: data.created
-            }
-        })
-        return {props: {problems: loadedProblem}}
+
+        try {
+            const res = await fetch(url)
+            const data = await res.json()
+            console.log(data, typeof(data))
+            const loadedProblem = data.map((data, index) => {
+                return {
+                    num: index + 1,
+                    title: data.title,
+                    slug: data.slug,
+                    difficulty: data.difficulty,
+                    description: data.description,
+                    created: data.created
+                }
+            })
+            return {props: {problems: loadedProblem}}
+        } catch(err) {
+            alert(err)
+        }
     }
 </script>
 
