32c510d6aaa02d0115303f93dad14bf254d3e78e
Jacob Kim <jpoly1219@gmail.com>
2021-12-14 10:08:44 +0000

    wrapped fetch in try catch

--- frontend/leetcode/src/routes/login.svelte
+++ frontend/leetcode/src/routes/login.svelte
@@ -18,12 +18,17 @@
             }
         }
         const url = "http://jpoly1219devbox.xyz:8090/auth/login"
-        const res = await fetch(url, options)
-        const accessToken = await res.json()
-        console.log(accessToken)
-        accessTokenStore.set(accessToken)
-        const payloadB64 = accessToken.split(".")[1]
-        timeToExpireStore.set(JSON.parse(window.atob(payloadB64)).exp)
+        
+        try {
+            const res = await fetch(url, options)
+            const accessToken = await res.json()
+            console.log(accessToken)
+            accessTokenStore.set(accessToken)
+            const payloadB64 = accessToken.split(".")[1]
+            timeToExpireStore.set(JSON.parse(window.atob(payloadB64)).exp)
+        } catch(err) {
+            alert(err)
+        }
     }
 </script>
 
