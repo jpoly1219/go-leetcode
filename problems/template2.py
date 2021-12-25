9d47b71a65d056cfcca45ad29b56a4b16fabdfa0
Jacob Kim <jpoly1219@gmail.com>
2021-12-25 11:26:20 +0000

    updated template2.py

--- problems/template2.py
+++ problems/template2.py
@@ -3,6 +3,36 @@ import json
 from typing import List
 
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def createLinkedList(nums):
+    nodeList = []
+    for num in nums:
+        newNode = ListNode(num)
+        nodeList.append(newNode)
+    
+    for i, node in enumerate(nodeList):
+        if i+1 == len(nodeList):
+            node.next = None
+        else:
+            node.next = nodeList[i+1]
+    
+    return nodeList
+
+
+def linkedListToVector(node):
+    intList = []
+    while (node != None):
+        intList.append(node.val)
+        node = node.next
+    
+    return intList
+    
+
 # insert Solution class here
 
 
@@ -13,20 +43,7 @@ sol = Solution()
 isOk = True
 # for each input pair, check if Solution.twoSum() returns a matching expected answer
 for i in range(len(data["input"]["nums"])):
-    answer = sol.twoSum(data["input"]["nums"][i], data["input"]["target"][i])
-    if answer != data["expected"][i]:
-        # write json to file
-        output = {
-            "result": "wrong",
-            "input": data["input"]["nums"][i],
-            "expected": data["expected"][i],
-            "output": answer
-        }
-        json_object = json.dumps(output, indent=4)
-        print(json_object)
-        
-        isOk = False
-        break
+    
 
 if isOk:
     # write json to file
