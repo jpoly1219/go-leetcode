52e244a13bcfba9086c483e3c729ebf56afd3ab4
Jacob Kim <jpoly1219@gmail.com>
2021-12-27 13:09:07 +0000

    updated template2.py

--- problems/template2.py
+++ problems/template2.py
@@ -36,23 +36,19 @@ with open("../testcase-2-add-two-numbers.json", "r") as read_file:
 sol = Solution()
 isOk = True
 
-for i in range(len(data["input"]["l1"])):
-    listL1 = data["input"]["l1"]
-    listL2 = data["input"]["l2"]
+listL1 = data["input"]["l1"]
+listL2 = data["input"]["l2"]
 
-    for i, nums in enumerate(listL1):
-        l1 = createLinkedList(listL1[i])
-        l2 = createLinkedList(listL2[i])
+for i, nums in enumerate(listL1):
+    l1 = createLinkedList(listL1[i])
+    l2 = createLinkedList(listL2[i])
 
-        print("l1: ", l1.val)
-        print("l2: ", l2.val)
-
-        solutionNode = sol.addTwoNumbers(l1, l2)
-        solutionList = linkedListToVector(solutionNode)
-        for j, solution in enumerate(solutionList):
-            if solution != data["expected"][j]:
-                isOk = False;
-                break;
+    solutionNode = sol.addTwoNumbers(l1, l2)
+    solutionList = linkedListToVector(solutionNode)
+    for solution in enumerate(solutionList):
+        if solutionList != data["expected"][i]:
+            isOk = False;
+            break;
 
 if isOk:
     # write json to file
