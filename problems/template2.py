0fa8f5ddfb55d374256bd2ae7c45b2ff5298989f
Jacob Kim <jpoly1219@gmail.com>
2021-12-27 11:30:57 +0000

    updated template2.py

--- problems/template2.py
+++ problems/template2.py
@@ -9,15 +9,11 @@ class ListNode:
         self.next = next
 
 
-def insertNode(head, i):
-    newNode = ListNode(i, head)
-    head = newNode
-
-
 def createLinkedList(nums):
     head = None
-    for num in nums:
-        insertNode(head, num)
+    for num in reversed(nums):
+        newNode = ListNode(num, head)
+        head = newNode
     
     return head
 
@@ -41,15 +37,22 @@ sol = Solution()
 isOk = True
 
 for i in range(len(data["input"]["l1"])):
-    l1 = createLinkedList(data["input"]["l1"])
-    l2 = createLinkedList(data["input"]["l2"])
+    listL1 = data["input"]["l1"]
+    listL2 = data["input"]["l2"]
 
-    solutionNode = sol.addTwoNumbers(l1[0], l2[0])
-    solutionList = linkedListToVector(solutionNode)
-    for j, solution in enumerate(solutionList):
-        if solution != data["expected"][j]:
-            isOk = False;
-            break;
+    for i, nums in enumerate(listL1):
+        l1 = createLinkedList(listL1[i])
+        l2 = createLinkedList(listL2[i])
+
+        print("l1: ", l1.val)
+        print("l2: ", l2.val)
+
+        solutionNode = sol.addTwoNumbers(l1, l2)
+        solutionList = linkedListToVector(solutionNode)
+        for j, solution in enumerate(solutionList):
+            if solution != data["expected"][j]:
+                isOk = False;
+                break;
 
 if isOk:
     # write json to file
