3fecf8d981402860c6989fc7a385745055796c2e
Jacob Kim <jpoly1219@gmail.com>
2021-12-29 14:48:22 +0000

    updated template2.js

--- problems/template2.js
+++ problems/template2.js
@@ -1,18 +1,60 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
+// template file
+const tc = require("../testcase-2-add-two-numbers.json")
 
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
+class ListNode {
+    constructor(val, next) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
 
-var addTwoNumbers = function(l1, l2) {
-    
-};
+function createLinkedList(nums) {
+    let head = null
+    nums.forEach(num => {
+        let newNode = new ListNode(num, head)
+        head = newNode
+    });
 
+    return head
+}
+
+function linkedListToVector(node) {
+    let intList = []
+    while (node != null) {
+        intList.push(node.val)
+        node = node.next
+    }
+
+    return intList
+}
+
+// insert Solution class here
+
+
+let isOk = true
+const listL1 = tc.input.l1
+const listL2 = tc.input.l2
+for (let i = 0; i < listL1.length; i++) {
+    const l1 = createLinkedList(listL1[i])
+    const l2 = createLinkedList(listL2[i])
+
+    const solutionNode = addTwoNumbers(l1, l2)
+    const solutionList = linkedListToVector(solutionNode)
+
+    for (let j = 0; j < solutionList.length; j++) {
+        if (solutionList[j] != tc.expected[i][j]) {
+            isOk = false
+            break
+        }   
+    }
+}
+
+if (isOk) {
+    const output = {
+        "result": "OK"
+    }
+    const data = JSON.stringify(output)
+    console.log(data)
+}
+
+console.log("test completed")
