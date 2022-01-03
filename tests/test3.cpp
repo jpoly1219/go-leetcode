210fd9f43fef6e760818189b8b7c49ac3c5c5516
Jacob Kim <jpoly1219@gmail.com>
2022-01-03 13:53:30 +0000

    test successful

--- tests/test3.cpp
+++ tests/test3.cpp
@@ -9,7 +9,27 @@ using namespace std;
 using json = nlohmann::json;
 
 // insert Solution class here
-
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.length();
+        
+        unordered_map<int, int>m;
+        int c = 0, maxlen = -1;
+        
+        for (int i = 0; i < n; i++) {
+            if (m.find(s[i]) == m.end() || (i - m[s[i]] > c))
+                c++;
+            else {
+                maxlen = max(maxlen, c);
+                c = i-m[s[i]];
+            }
+            m[s[i]] = i;
+        }
+        
+        return max(maxlen, c);
+    }
+};
 
 int main() {
     Solution sol;
