956d1615c0b774914aea3f4f44ec77379ff2de86
Jacob Kim <jpoly1219@gmail.com>
2022-01-04 13:10:38 +0000

    test3 successful

--- tests/test3.py
+++ tests/test3.py
@@ -4,7 +4,18 @@ from typing import List
 
 
 # insert Solution class here
-
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s) == 0:
+            return 0
+        res = 1
+        slow = 0
+        for i in range(1, len(s)):
+            if s[i] in s[slow : i]:
+                slow = s[slow:i].index(s[i]) + slow + 1
+                #print(slow)
+            res = max(res, i - slow + 1)
+        return res
 
 with open("tc3.json", "r") as read_file:
     data = json.load(read_file)
