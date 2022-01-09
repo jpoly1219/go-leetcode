e80d2989b255a369a8e6f17483e46d2259c44618
Jacob Kim <jpoly1219@gmail.com>
2022-01-09 04:05:19 +0000

    created template5.js

---
+++ problems/template5.js
@@ -0,0 +1,28 @@
+// template file
+const tc = require("./tc5.json")
+
+// insert Solution class here
+
+
+let isOk = true
+const arrInput = tc.input
+const arrExpected = tc.expected
+for (let i = 0; i < arrInput.length; i++) {
+    const expected = arrExpected[i]
+    const solution = longestPalindrome(arrInput[i])
+    if (!expected.includes(solution)) {
+        isOk = false
+        break
+    }
+
+}
+
+if (isOk) {
+    const output = {
+        "result": "OK"
+    }
+    const data = JSON.stringify(output)
+    console.log(data)
+}
+
+console.log("test completed")
