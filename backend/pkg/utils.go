d2dbeefc520a69c78213618bed7d5727a1d82586
Jacob Kim <jpoly1219@gmail.com>
2022-01-15 14:19:25 +0000

    writeLog won't need an error at this point

--- backend/pkg/utils.go
+++ backend/pkg/utils.go
@@ -5,16 +5,11 @@ import (
 	"os"
 )
 
-func writeLog(message string, err error) error {
-	f, err := os.OpenFile("/", os.O_APPEND, 0644)
-	if err != nil {
-		return err
-	}
+func writeLog(message string, err error) {
+	f, _ := os.OpenFile("/backend/logs.txt", os.O_APPEND, 0644)
 	defer f.Close()
 
 	log.SetOutput(f)
 	log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
 	log.Println(message, err)
-
-	return nil
 }
