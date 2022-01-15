f5ca09f79111afe906e58239d557300bb4bf46b6
Jacob Kim <jpoly1219@gmail.com>
2022-01-15 14:13:56 +0000

    created writeLog() to write logs to file

--- backend/pkg/utils.go
+++ backend/pkg/utils.go
@@ -1,1 +1,20 @@
 package pkg
+
+import (
+	"log"
+	"os"
+)
+
+func writeLog(message string, err error) error {
+	f, err := os.OpenFile("/", os.O_APPEND, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+	log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
+	log.Println(message, err)
+
+	return nil
+}
