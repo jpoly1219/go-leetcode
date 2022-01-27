93285a716de7cde76e4ad93fab96b00bd1085347
Jacob Kim <jpoly1219@gmail.com>
2022-01-27 23:31:39 +0900

    profile page now calls the API for user data

--- frontend/leetcode/src/routes/profile.svelte
+++ frontend/leetcode/src/routes/profile.svelte
@@ -1,10 +1,39 @@
-<script>
+<script context="module">
+    import { get } from "svelte/store"
+    import { accessTokenStore } from "../stores/stores.js"
 
+    export async function load() {
+        let accessToken = get(accessTokenStore)
+        let username
+        if (accessToken != "") {
+            const payloadB64 = accessToken.split(".")[1]
+            username = JSON.parse(window.atob(payloadB64)).username
+        }
+
+        const url = `http://localhost:8090/users/${username}`
+        const options = {
+            method: "GET"
+        }
+
+        try {
+            const res = await fetch(url, options)
+            const userData = await res.json()
+
+            return {props: {user: userData}}
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+</script>
+
+<script>
+    export let user
 </script>
 
 <svelte:head>Profile - go-leetcode</svelte:head>
 
 <div class="container">
-    <p>This is the profile page.</p>
+    <p>This is the profile page of the user {user}.</p>
     <p>Data to be added soon.</p>
 </div>
