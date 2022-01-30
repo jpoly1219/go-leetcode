6f67317e2af638cbaca1443592b87d1cc3a1aa87
Jacob Kim <jpoly1219@gmail.com>
2022-01-30 23:33:08 +0900

    dbinit now adds templates for problem 1

--- dbinit.sql
+++ dbinit.sql
@@ -313,23 +313,186 @@ INSERT INTO problems (title, slug, difficulty, description) VALUES (
 );
 
 -- create templates
+-- template 1
 INSERT INTO templates (lang, slug, template) VALUES (
+    'cpp',
+    '1-two-sum',
+    $$// template file
+    #include <fstream>
+    #include <iostream>
+    #include <vector>
+    #include "json.hpp"
 
+    using namespace std;
+    using json = nlohmann::json;
+
+    // insert Solution class here
+
+    int main() {
+        Solution sol;
+
+        ifstream i("../testcase-1-two-sum.json");
+        json j;
+        i >> j;
+
+        vector<vector<int>> vecNums = j["input"]["nums"];
+        vector<int> vecTargets = j["input"]["target"];
+        vector<vector<int>> vecExpected = j["expected"];
+
+        bool isOk = true;
+        // test
+        for (int i = 0; i < vecNums.size(); i++) {
+            vector<int> vecSolution = sol.twoSum(vecNums.at(i), vecTargets.at(i));
+            if (vecSolution != vecExpected.at(i)) {
+                json output = {
+                    {"result", "wrong"},
+                    {"input", vecNums.at(i)},
+                    {"expected", vecExpected.at(i)},
+                    {"output", vecSolution}
+                };
+                cout << output.dump(4) << endl;
+                isOk = false;
+                break;
+            }
+        }
+        if (isOk) {
+            json output = {
+                {"result", "OK"}
+            };
+            cout << output.dump(4) << endl;
+        }
+        cout << "test completed" << endl;
+        i.close();
+    }$$
 );
 
 INSERT INTO templates (lang, slug, template) VALUES (
+    'java',
+    '1-two-sum',
+    $$// template file
+    // insert Solution class here
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            int[nums.length] output;
+            for (int i = 0; i < nums.length; i++) {
+                output[i] = nums[i] + target;
+            }
+            return output;
+        }
+    }
 
+    public class Template {
+        public static void main(String[] args) {
+            Solution sol = new Solution();
+            boolean isOk = true;
+            for (int i = 0; i < )
+        }
+    }$$
 );
 
 INSERT INTO templates (lang, slug, template) VALUES (
+    'js',
+    '1-two-sum',
+    $$// template file
+    const tc = require("../testcase-1-two-sum.json")
 
+    // insert Solution class here
+
+    let isOk = true
+    for (let i = 0; i < tc.input.nums.length; i++) {
+        const answer = twoSum(tc.input.nums[i], tc.input.target[i])
+        if (answer != tc.expected[i]) {
+            const output = {
+                "result": "wrong",
+                "input": tc.input.nums[i],
+                "expected": tc.expected[i],
+                "output": answer
+            }
+            const data = JSON.stringify(output)
+            console.log(data)
+            isOk = false
+            break
+        }
+    }
+
+    if (isOk) {
+        const output = {
+            "result": "OK"
+        }
+        const data = JSON.stringify(output)
+        console.log(data)
+    }
+
+    console.log("test completed")$$
 );
 
 INSERT INTO templates (lang, slug, template) VALUES (
+    'py',
+    '1-two-sum',
+    $$# template file
+    import json
+    from typing import List
+
+
+    # insert Solution class here
+
+
+    with open("../testcase-1-two-sum.json", "r") as read_file:
+        data = json.load(read_file)
+
+    sol = Solution()
+    isOk = True
+    # for each input pair, check if Solution.twoSum() returns a matching expected answer
+    for i in range(len(data["input"]["nums"])):
+        answer = sol.twoSum(data["input"]["nums"][i], data["input"]["target"][i])
+        if answer != data["expected"][i]:
+            # write json to file
+            output = {
+                "result": "wrong",
+                "input": data["input"]["nums"][i],
+                "expected": data["expected"][i],
+                "output": answer
+            }
+            json_object = json.dumps(output, indent=4)
+            print(json_object)
+            
+            isOk = False
+            break
+
+    if isOk:
+        # write json to file
+        output = {
+            "result": "OK"
+        }
+        json_object = json.dumps(output, indent=4)
+        print(json_object)
+
+    print("test completed")
+
+    $$
+);
+
+INSERT INTO templates (lang, slug, template)
+
+-- template 2
+INSERT INTO templates (lang, slug, tempalte) VALUES (
 
 );
 
-INSERT INTO templates (lang, slug, template) VALUES (
+INSERT INTO templates (lang, slug, tempalte) VALUES (
 
 );
+
+INSERT INTO templates (lang, slug, tempalte) VALUES (
+
+);
+
+INSERT INTO templates (lang, slug, tempalte) VALUES (
+
+);
+
+INSERT INTO templates (lang, slug, tempalte) VALUES (
+
+);
+
 -- create testcases
