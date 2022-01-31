6fee1c85106ca2ab14f989ac8043785044706104
Jacob Kim <jpoly1219@gmail.com>
2022-01-31 22:30:46 +0900

    dbinit now adds templates for problem 3

--- dbinit.sql
+++ dbinit.sql
@@ -475,7 +475,7 @@ INSERT INTO templates (lang, slug, template) VALUES (
 INSERT INTO templates (lang, slug, template)
 
 -- template 2
-INSERT INTO templates (lang, slug, tempalte) VALUES (
+INSERT INTO templates (lang, slug, template) VALUES (
     'cpp',
     '2-add-two-numbers',
     $$// template file
@@ -559,7 +559,7 @@ INSERT INTO templates (lang, slug, tempalte) VALUES (
     }$$
 );
 
-INSERT INTO templates (lang, slug, tempalte) VALUES (
+INSERT INTO templates (lang, slug, template) VALUES (
     'js',
     '2-add-two-numbers',
     $$// template file
@@ -624,7 +624,7 @@ INSERT INTO templates (lang, slug, tempalte) VALUES (
     console.log("test completed")$$
 );
 
-INSERT INTO templates (lang, slug, tempalte) VALUES (
+INSERT INTO templates (lang, slug, template) VALUES (
     'py',
     '2-add-two-numbers',
     $$# template file
@@ -688,8 +688,130 @@ INSERT INTO templates (lang, slug, tempalte) VALUES (
         print(json_object)
 
     print("test completed")
-    
+
     $$
 );
+
+-- template 3
+INSERT INTO templates (lang, slug, template) VALUES (
+    'cpp',
+    '3-longest-substring-without-repeating-characters',
+    $$// template file
+    #include <fstream>
+    #include <iostream>
+    #include <vector>
+    #include <string>
+    #include "json.hpp"
+
+    using namespace std;
+    using json = nlohmann::json;
+
+    // insert Solution class here
+
+
+    int main() {
+        Solution sol;
+
+        ifstream i("../testcase-3-longest-substring-without-repeating-characters.json");
+        json j;
+        i >> j;
+
+        vector<string> vecInput = j["input"];
+        vector<int> vecExpected = j["expected"];
+
+        bool isOk = true;
+        // test
+        for (size_t i = 0; i < vecInput.size(); i++) {
+            int solution = sol.lengthOfLongestSubstring(vecInput.at(i));
+            if (solution != vecExpected.at(i)) {
+                isOk = false;
+                break;
+            }
+        }
+        if (isOk) {
+            json output = {
+                {"result", "OK"}
+            };
+            cout << output.dump(4) << endl;
+        }
+        cout << "test completed" << endl;
+        i.close();
+    }$$
+);
+
+INSERT INTO templates (lang, slug, template) VALUES (
+    'js',
+    '3-longest-substring-without-repeating-characters',
+    $$// template file
+    const tc = require("../testcase-3-longest-substring-without-repeating-characters.json")
+
+    // insert Solution class here
+
+
+    let isOk = true
+    const arrInput = tc.input
+    const arrExpected = tc.expected
+    for (let i = 0; i < arrInput.length; i++) {
+        const solution = lengthOfLongestSubstring(arrInput[i])
+        if (solution != arrExpected[i]) {
+            isOk = false
+            break
+        }
+
+    }
+
+    if (isOk) {
+        const output = {
+            "result": "OK"
+        }
+        const data = JSON.stringify(output)
+        console.log(data)
+    }
+
+    console.log("test completed")$$
+);
+
+INSERT INTO templates (lang, slug, template) VALUES (
+    'py',
+    '3-longest-substring-without-repeating-characters',
+    $$# template file
+    import json
+    from typing import List
+
+
+    # insert Solution class here
+
+
+
+    with open("../testcase-3-longest-substring-without-repeating-characters.json", "r") as read_file:
+        data = json.load(read_file)
+
+    sol = Solution()
+    isOk = True
+
+    listInput = data["input"]
+    listExpected = data["expected"]
+
+    for i, element in enumerate(listInput):
+        solution = sol.lengthOfLongestSubstring(element)
+        if solution != listExpected[i]:
+            isOk = False
+            break
+
+    if isOk:
+        output = {
+            "result": "OK"
+        }
+        json_object = json.dumps(output, indent=4)
+        print(json_object)
+
+    print("test completed")
+
+    $$
+);
+
+-- template 4
+
+-- template 5
 
 -- create testcases
